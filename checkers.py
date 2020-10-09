2dd1e97cc3671aa74283e4579b3ec3acbc199243
David Link <dvlink@gmail.com>
2020-10-08 23:31:28 -0400

    making board output nice

--- checkers.py
+++ checkers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 NUM_ROWS = NUM_COL = 8
-
 
 class Checkers(object):
     '''A checkers program'''
@@ -15,14 +15,32 @@ class Checkers(object):
         self.showBoard()
 
     def _createBoard(self):
+        i = 0
         for x in range(NUM_ROWS):
+            i += 1
             row = []
             for cell in range(NUM_COL):
-                row.append('x')
+                if i in (1, 2):
+                    row.append(u'◯')
+                elif i in (7, 8):
+                    row.append(u'✪')
+                else:
+                    row.append(u' ')
             self.board.append(row)
 
     def showBoard(self):
-        print self.board
+        board_str = u''
+        board_str += u'┌───┬───┬───┬───┬───┬───┬───┬───┐\n'
+        i = 0
+        for row in self.board:
+            i += 1
+            for cell in row:
+                board_str += u'│ %s ' % cell
+            board_str += u'│\n'
+            if i < NUM_ROWS:
+                board_str += u'├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+        board_str += u'└───┴───┴───┴───┴───┴───┴───┴───┘\n'
+        print board_str
 
 if __name__ == '__main__':
     Checkers().run()
