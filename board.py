07409ebbaa3c5137b8ba0820cfc26dadff6be2e9
David Link <dvlink@gmail.com>
2022-02-11 22:57:55 +0000

    moved board code into board module

--- board.py
+++ board.py
@@ -1,5 +1,38 @@
-# this module will contain the board related logic
+# -*- coding: utf-8 -*-
 
+NUM_ROWS = NUM_COL = 8
 
 class Board(object):
-    pass
+
+    def __init__(self):
+        self.board = []
+        self._createBoard()
+
+    def _createBoard(self):
+        i = 0
+        for x in range(NUM_ROWS):
+            i += 1
+            row = []
+            for cell in range(NUM_COL):
+                if i in (1, 2):
+                    row.append(u'◯')
+                elif i in (7, 8):
+                    row.append(u'✪')
+                else:
+                    row.append(u' ')
+            self.board.append(row)
+
+    def show(self):
+        board_str = u''
+        board_str += u'┌───┬───┬───┬───┬───┬───┬───┬───┐\n'
+        i = 0
+        for row in self.board:
+            i += 1
+            for cell in row:
+                board_str += u'│ %s ' % cell
+            board_str += u'│\n'
+            if i < NUM_ROWS:
+                board_str += u'├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+        board_str += u'└───┴───┴───┴───┴───┴───┴───┴───┘\n'
+        print board_str
+
--- checkers.py
+++ checkers.py
@@ -1,46 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-NUM_ROWS = NUM_COL = 8
+from board import Board
 
 class Checkers(object):
     '''A checkers program'''
 
     def __init__(self):
-        self.board = []
+        self.board = Board()
 
     def run(self):
         print 'run'
-        self._createBoard()
-        self.showBoard()
-
-    def _createBoard(self):
-        i = 0
-        for x in range(NUM_ROWS):
-            i += 1
-            row = []
-            for cell in range(NUM_COL):
-                if i in (1, 2):
-                    row.append(u'◯')
-                elif i in (7, 8):
-                    row.append(u'✪')
-                else:
-                    row.append(u' ')
-            self.board.append(row)
-
-    def showBoard(self):
-        board_str = u''
-        board_str += u'┌───┬───┬───┬───┬───┬───┬───┬───┐\n'
-        i = 0
-        for row in self.board:
-            i += 1
-            for cell in row:
-                board_str += u'│ %s ' % cell
-            board_str += u'│\n'
-            if i < NUM_ROWS:
-                board_str += u'├───┼───┼───┼───┼───┼───┼───┼───┤\n'
-        board_str += u'└───┴───┴───┴───┴───┴───┴───┴───┘\n'
-        print board_str
+        self.board.show()
 
 if __name__ == '__main__':
     Checkers().run()
